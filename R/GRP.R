90cd4230da780dc018e9a5fccf439f2629766d62
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2019-11-13 23:17:36 +0100

    updating GRP, making atomic groups a named list.

--- R/GRP.R
+++ R/GRP.R
@@ -1,6 +1,7 @@
 Cuniqlengths <- data.table:::Cuniqlengths
 Cfrank <- data.table:::Cfrank
 forderv <- data.table:::forderv
+
 # or getGRP: more speed using tabulate ?? or code your own??
 GRP <- function(X, by = NULL, sort = TRUE, order = 1L,
                 na.last = FALSE, return.groups = TRUE, return.order = FALSE) {
@@ -8,33 +9,44 @@ GRP <- function(X, by = NULL, sort = TRUE, order = 1L,
 }
 
 # To do: use data.table:::CsubsetDT and CsubsetVector !!
-GRP.default <- function(X, by = NULL, sort = TRUE, order = 1L,
-                na.last = FALSE, return.groups = TRUE, return.order = FALSE) { # , gs = TRUE # o
+GRP.default <- function(X, by = NULL, sort = TRUE, order = 1L, na.last = FALSE,
+                        return.groups = TRUE, return.order = FALSE) { # , gs = TRUE # o
+
+  call <- match.call()
+
   if(is.list(X) && is.null(by)) {
     by <- seq_along(X)
     namby <- names(X)
   } else if(is.call(by)) {
     namby <- by <- all.vars(by)
   } else if(is.null(by)) {
-    namby <- deparse(substitute(X))
+    namby <- paste(all.vars(call), collapse = ".")  # deparse(substitute(X)) # all.vars is faster !!!
   } else namby <- names(X)[by]
+
   o <- forderv(X, by, TRUE, sort, order, na.last) # faster calling c?? -> nah, the checks implemented in forderv are good  and efficient !!
   f <- attr(o, "starts") # , exact = TRUE -> slightly slower
+
   if(length(o)) { # if ordered, returns 0
     len <- .Call(Cuniqlengths, f, length(o)) # len = data.table:::uniqlengths(f, length(o))
     grpuo <- .Call(Cfrank, o, f, len, "dense") #-1L # grpuo = rep.int(seq_along(len),len)[data.table:::forderv(o)] # faster way?? -> try to speed this up..
-    ordered <- c(GRP.sort = sort, initially.ordered = TRUE)
+    ordered <- c(GRP.sort = sort, initially.ordered = FALSE)
+    if(return.groups) {
+      groups <- if(inherits(X, "data.table")) # better or faster way ??
+        X[o[f], by, with = FALSE] else if(inherits(X, "data.frame"))
+        X[o[f], by, drop = FALSE] else if(is.atomic(X))
+        `names<-`(list(X[o[f]]), namby) else qDF(X)[o[f], by, drop = FALSE]
+    } else groups <- NULL
   } else {
     len <- .Call(Cuniqlengths, f, NROW(X)) # or cumsubtract !!!
     grpuo <- rep.int(seq_along(len), len) # rep.int fastest ?? -> about same speed as rep !!  # o = f # for unique. right???
-    ordered <- c(GRP.sort = sort, initially.ordered = FALSE)
+    ordered <- c(GRP.sort = sort, initially.ordered = TRUE)
+    if(return.groups) {
+      groups <- if(inherits(X, "data.table")) # better or faster way ??
+      X[f, by, with = FALSE] else if(inherits(X, "data.frame"))
+      X[f, by, drop = FALSE] else if(is.atomic(X))
+      `names<-`(list(X[f]), namby) else qDF(X)[f, by, drop = FALSE]
+    } else groups <- NULL
   }
-  if(return.groups) {
-    groups <- if(inherits(X, "data.table")) # better or faster way ??
-              X[o[f], by, with = FALSE] else if(inherits(X, "data.frame"))
-              X[o[f], by, drop = FALSE] else if(is.atomic(X))
-              X[o[f]] else qDF(X)[o[f], by, drop = FALSE]
-  } else groups <- NULL
   if(!return.order) o <- NULL
   return(`class<-`(list(N.groups = length(f),
                         group.id = grpuo,
@@ -43,14 +55,16 @@ GRP.default <- function(X, by = NULL, sort = TRUE, order = 1L,
                         group.vars = namby,
                         ordered = ordered,
                         order = o,
-                        call = match.call()), "GRP"))
+                        call = call), "GRP"))
 }
 
 is.GRP <- function(x) inherits(x, "GRP")
 
-group.names.GRP <- function(g) { # fastest !!!
+group.names.GRP <- function(g, force.char = FALSE) { # fastest !!!
   groups <- g[[4L]]
-  if(is.atomic(groups)) groups else do.call(paste,c(groups, list(sep = ".")))
+  if(length(groups) == 1L) {
+   if(force.char && !is.character(groups[[1L]])) paste0(groups[[1L]]) else groups[[1L]]
+  } else do.call(paste, c(groups, list(sep = ".")))
 }
 
 print.GRP <- function(g, n = 6) {
@@ -63,7 +77,8 @@ print.GRP <- function(g, n = 6) {
   if(!is.null(g[[4L]])) {
     ug <- g[[4L]]
     cat("\nGroups with sizes: ", fill = TRUE)
-    if(is.atomic(ug)) {
+    if(length(ug) == 1L) {
+      ug <- ug[[1L]]
       if(length(ug) > 2L*n) {
         ind <- seq.int(g[[1L]]-n+1L, g[[1L]])
         print.default(setNames(g[[3L]][1:n], ug[1:n]))
@@ -104,45 +119,60 @@ as.factor.GRP <- function(g) {
   if(is.null(gr)) {
     attr(f, "levels") <- as.character(seq_len(g[[1L]]))
   } else {
-    attr(f, "levels") <- if(is.atomic(gr)) as.character(gr) else # or formatC ??
-                         do.call(paste, c(gr, list(sep = ".")))
+    if(length(gr) == 1L) {
+      attr(f, "levels") <- if(is.character(gr[[1L]])) gr[[1L]] else as.character(gr[[1L]]) # or formatC ??
+    } else {
+      attr(f, "levels") <- do.call(paste, c(gr, list(sep = ".")))
+    }
   }
   class(f) <- if(any(g[[6L]])) c("ordered","factor") else "factor"
   return(f)
 }
 
 GRP.qG <- function(X) {
+  # nam <- deparse(substitute(X)) # takes 9 microseconds !!, all vars on call is faster !!
   ng <- attr(X, "N.groups")
   ordered <- if(is.ordered(X)) c(TRUE,TRUE) else c(FALSE,FALSE)
   attributes(X) <- NULL
+  call <- match.call()
   return(`class<-`(list(N.groups = ng,
                         group.id = X,
                         group.sizes = .Internal(tabulate(X, ng)),
                         groups = NULL,
-                        group.vars = NULL,
+                        group.vars = paste(all.vars(call), collapse = "."),
                         ordered = ordered,
                         order = NULL,
-                        call = match.call()), "GRP"))
+                        call = call), "GRP"))
 }
 
 GRP.factor <- function(X) {
+  # nam <- deparse(substitute(X)) # takes 9 microseconds !!
   lev <- attr(X, "levels")
   nl <- length(lev)
   ordered <- if(is.ordered(X)) c(TRUE,TRUE) else c(FALSE,FALSE)
   attributes(X) <- NULL
+  call <- match.call()
+  nam <- paste(all.vars(call), collapse = ".")
   return(`class<-`(list(N.groups = nl,
                         group.id = X,
                         group.sizes = .Internal(tabulate(X, nl)), # faster tabulating factors ??? -> Nope, same speed of nl is supplied !!
-                        groups = lev,
-                        group.vars = NULL,
+                        groups = `names<-`(list(lev), nam),
+                        group.vars = nam,
                         ordered = ordered,
                         order = NULL,
-                        call = match.call()), "GRP"))
+                        call = call), "GRP"))
 }
 
 GRP.pseries <- function(X) {
   g <- attr(X, "index") # index cannot be atomic since plm always adds a time variable !!
-  g <- if(length(g) > 2L)  interaction(g[-length(g)], drop = TRUE) else g[[1L]]
+  if(length(g) > 2L) {
+    mlg <- -length(g)
+    nam <- paste(names(g)[mlg], collapse = ".")
+    g <- interaction(g[mlg], drop = TRUE)
+  } else {
+    nam <- names(g)[1L]
+    g <- g[[1L]]
+  }
   lev <- attr(g, "levels")
   nl <- length(lev)
   ordered <- if(is.ordered(g)) c(TRUE,TRUE) else c(FALSE,FALSE)
@@ -150,8 +180,8 @@ GRP.pseries <- function(X) {
   return(`class<-`(list(N.groups = nl,
                         group.id = g,
                         group.sizes = .Internal(tabulate(g, nl)), # faster tabulating factors ??? -> Nope, same speed of nl is supplied !!
-                        groups = lev,
-                        group.vars = NULL,
+                        groups = `names<-`(list(lev), nam),
+                        group.vars = nam,
                         ordered = ordered,
                         order = NULL,
                         call = match.call()), "GRP"))
