1bce5e801019f506f3cbe9a04a0ebe509045e899
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2019-11-16 15:45:17 +0100

    adding Lsubset function based on Rcpp::sugar indexing. Faster than data.frame on medium sized data.frames, but slower on large ones.

--- src/small_helper.cpp
+++ src/small_helper.cpp
@@ -47,6 +47,95 @@ void cond_duplattributes(SEXP x, SEXP y) {
   if(TYPEOF(x) == TYPEOF(y)) DUPLICATE_ATTRIB(x, y); // SET_ATTRIB(x, ATTRIB(y));
 }
 
+// [[Rcpp::export]] // not faster than base method on large data (PRIO)!!
+List LSubset(const List& x, const IntegerVector& rows = 0, const IntegerVector& cols = 0) {
+  int rs = rows.size(), cs = cols.size(), l = x.size();
+  int outs = (cs == 1 && cols[0] == 0) ? l : cs;
+  List out(outs);
+  if(outs == l && rows[0] != 0) {
+    for(int j = l; j--; ) {
+      switch(TYPEOF(x[j])) {
+      case REALSXP: {
+        NumericVector column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      case INTSXP: {
+        IntegerVector column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      case STRSXP: {
+        CharacterVector column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      case LGLSXP: {
+        LogicalVector column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      case CPLXSXP: {
+        ComplexVector column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      case VECSXP: {
+        List column = x[j];
+        out[j] = column[rows];
+        break;
+      }
+      default: stop("Not supported SEXP type!");
+      }
+      SHALLOW_DUPLICATE_ATTRIB(out[j], x[j]);
+    }
+    // DUPLICATE_ATTRIB(out, x);
+    // if(Rf_isFrame(x)) out.attr("row.names") = wrap(x.attr("row.names"))[rows];
+  } else if(outs != l && rows[0] != 0) {
+    for(int j = cs; j--; ) {
+      int col = cols[j]-1;
+      switch(TYPEOF(x[col])) {
+      case REALSXP: {
+        NumericVector column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      case INTSXP: {
+        IntegerVector column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      case STRSXP: {
+        CharacterVector column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      case LGLSXP: {
+        LogicalVector column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      case CPLXSXP: {
+        ComplexVector column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      case VECSXP: {
+        List column = x[col];
+        out[j] = column[rows];
+        break;
+      }
+      default: stop("Not supported SEXP type!");
+      }
+      SHALLOW_DUPLICATE_ATTRIB(out[j], x[col]);
+    }
+  } else if(rs == 1 && rows[0] == 0) {
+   out = x[cols-1];
+  } else stop("Must subset either rows or columns or both!");
+  return out;
+}
+
+
 // // [[Rcpp::export]] // needed?? for what ??
 // void sduplattributes(SEXP x, SEXP y) {
 //   SHALLOW_DUPLICATE_ATTRIB(x, y); // DUPLICATE_ATTRIB(x, y);
