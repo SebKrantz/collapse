d1a0f72d0240dfec0557218d82772c678ab67688
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2019-12-21 14:22:07 +0100

    macros up to date.

--- R/global_macros.R
+++ R/global_macros.R
@@ -4,49 +4,26 @@
                       "select-replace-vars","quick-conversion","advanced-data-aggregation",
                       "data-transformations","time-series-panel-series","list-processing",
                       "quick-summary","recode-replace","small-helpers")
-#
-# obj <- objects("package:collapse")
-# export <- setdiff(obj[-grep("Cpp|_collapse_", obj)],
-#                   c("change_row_names","cols2int","cond_duplicate_attributes","at2GRP",
-#                     "G_t","give_nam","library.dynam.unload","remove_attributes","setRow.names",
-#                     "system.file","TRAtoInt","View.qsu","group.names.GRP","na.rm","pwcor","pwcov",
-#                     "seq_col","seq_row","setAttributes","setColnames","setDimnames","setRownames","duplAttributes",
-#                     "fnlevels","anyNAerror","charorNULL","collapse_init","complete.cases","cols2log",
-#                     "colsubset","cond_duplattributes","cond_duplAttributes","condsetn","cor","cov","Csplit",
-#                     "duplattributes","hist","list_extract_FUN","list_extract_ind","list_extract_names",
-#                     "list_extract_regex","myModFrame","NCOL2","NROW2","par","plot","rbindlist","as.formula",
-#                     "forder","forderv","getfl","getPartData","init_collapse","rgrep","setAttr","setattr_clp",
-#                     "setattributes","setcolorder","setNames","subsetDT","subsetfl","subsetVector","terms.formula",
-#                     "uniqlengths","unique_factor","frank"))
-# cat(paste0("export(", export,")\n"))
-#
-# methods <- obj[grep(".default|.matrix|.data.frame|.pseries|.pdata.frame|.grouped_df|GRP.|print.|plot.", obj)]
-# methods <- unlist(lapply(strsplit(methods,".", fixed = TRUE), function(x) paste0("S3method(",x[1],", ",paste(x[-1], collapse = "."),")\n" )))
-# cat(methods)
 
+.COLLAPSE_ALL    <-      c("%!in%","add_stub","all.identical","as.factor.GRP","as.numeric_factor",
+                           "atomic_elem","atomic_elem<-","B","BY","cat_vars","cat_vars<-","char_vars",
+                           "char_vars<-","collap","D","dapply","Date_vars","Date_vars<-","F","fact_vars",
+                           "fact_vars<-","fbetween","fdiff","ffirst","fgrowth","fHDbetween","fHDwithin",
+                           "flag","flast","fmax","fmean","fmedian","fmin","fmode","fNdistinct","fnlevels",
+                           "fNobs","fprod","fscale","fsd","fsum","funique","fvar","fwithin","G","get_elem",
+                           "get_vars","get_vars<-","GGDC10S","group_names.GRP","GRP","has_elem","HDB","HDW",
+                           "irreg_elem","is.categorical","is.Date","is.GRP","is.regular","is.unlistable",
+                           "L","ldepth","list_elem","list_elem<-","logi_vars","logi_vars<-","mctl","mrtl",
+                           "na_insert","na_rm","namlab","num_vars","num_vars<-","psacf","psccf","psmat",
+                           "pspacf","pwcor","pwcov","qDF","qDT","qF","qG","qM","qsu","rapply2d","Recode",
+                           "reg_elem","replace_non_finite","replace_outliers","seq_col","seq_row",
+                           "setColnames","setDimnames","setRownames","STD","TRA","unlist2d","vlabels",
+                           "vlabels<-","W","wlddev")
 
-
-.COLLAPSE_ALL    <-      c("add_stub","all.identical","as.factor.GRP","atomic_elem",
-                           "atomic_elem<-","B","BY","cat_vars","cat_vars<-","char_vars",
-                           "char_vars<-","collap","D","dapply","Date_vars","Date_vars<-",
-                           "F","fact_vars","fact_vars<-","fdiff","ffirst","fgrowth","flag",
-                           "flast","fmax","fmean","fmedian","fmin","fmode","fNdistinct",
-                           "fNobs","fprod","fscale","fsd","fsum","fvar","fbetween","fwithin",
-                           "fHDbetween","fHDwithin","Recode","replace_non_finite","replace_outliers",
-                           "G","get_elem",
-                           "get_vars","get_vars<-","GGDC","group_names.GRP","GRP","has_elem",
-                           "HDB","HDW","irreg_elem","is.categorical","is.Date","is.GRP",
-                           "is.regular","is.unlistable","L","ldepth","list_elem",
-                           "list_elem<-","logi_vars","logi_vars<-","mctl","mrtl","namlab",
-                           "num_vars","num_vars<-","psacf","psccf","psmat","pspacf","qDF",
-                           "qDT","qF","qG","qM","qsu","rapply2d","reg_elem","STD","TRA",
-                           "unlist2d","vlabels","vlabels<-","W","wlddev")
-
-.COLLAPSE_GENERIC   <-   c("B","BY","D","F","fdiff","ffirst","fgrowth","flag",
-                           "flast","fmax","fmean","fmedian","fmin","fmode","fNdistinct",
-                           "fNobs","fprod","fscale","fsd","fsum","fvar",
-                           "fbetween","fwithin","fHDbetween","fHDwithin","G","GRP",
-                           "HDB","HDW","L","psacf","psccf","psmat","pspacf","qsu",
+.COLLAPSE_GENERIC   <-   c("B","BY","D","F","fbetween","fdiff","ffirst","fgrowth","fHDbetween",
+                           "fHDwithin","flag","flast","fmax","fmean","fmedian","fmin","fmode",
+                           "fNdistinct","fNobs","fprod","fscale","fsd","fsum","fvar","fwithin",
+                           "G","GRP","HDB","HDW","L","psacf","psccf","psmat","pspacf","qsu",
                            "STD","TRA","W")
 
 .COLLAPSE_DATA <- c("GGDC10S","wlddev")
