46f3ee8a3fa1cbdfbcf49dc52d2415cb1eea8e2f
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-03-24 00:28:22 +0100

    Adding more tests. testing add_vars.

--- tests/testthat/test-select-replace-vars.R
+++ tests/testthat/test-select-replace-vars.R
@@ -1,10 +1,12 @@
-context("select_replace vars")
+context("select, replace or add vars")
 
 test_that("selecting vars works well", {
   expect_identical(get_vars(wlddev, 4:8), wlddev[4:8])
   expect_identical(get_vars(wlddev, -(4:8)), wlddev[-(4:8)])
   expect_identical(get_vars(wlddev, sapply(wlddev, is.numeric)), wlddev[sapply(wlddev, is.numeric)])
   expect_identical(get_vars(wlddev, c("iso3c","PCGDP","ODA")), wlddev[c("iso3c","PCGDP","ODA")])
+  expect_identical(get_vars(wlddev, "D", regex = TRUE), wlddev[c("OECD","PCGDP","ODA")])
+  expect_identical(get_vars(wlddev, c("D","L"), regex = TRUE), wlddev[c("OECD","PCGDP","LIFEEX","ODA")])
   expect_identical(get_vars(wlddev, is.factor), wlddev[sapply(wlddev, is.factor)])
 
   expect_identical(num_vars(wlddev), wlddev[sapply(wlddev, is.numeric)])
@@ -29,6 +31,14 @@ test_that("replacing vars works well", {
 
   wlddevold <- wlddev
   get_vars(wlddev, c("iso3c","PCGDP","ODA")) <- get_vars(wlddev, c("iso3c","PCGDP","ODA"))
+  expect_identical(wlddevold, wlddev)
+
+  wlddevold <- wlddev
+  get_vars(wlddev, "D", regex = TRUE) <- get_vars(wlddev, "D", regex = TRUE)
+  expect_identical(wlddevold, wlddev)
+
+  wlddevold <- wlddev
+  get_vars(wlddev, c("D","L"), regex = TRUE) <- get_vars(wlddev, c("D","L"), regex = TRUE)
   expect_identical(wlddevold, wlddev)
 
   wlddevold <- wlddev
@@ -64,11 +74,63 @@ test_that("replacing vars works well", {
   expect_identical(wlddevold, wlddev)
 })
 
+test_that("adding vars works well", {
+  wlddev1 <- wlddev2 <- wlddev
+  temp <- STD(get_vars(wlddev, 9:12))
+  add_vars(wlddev1) <- temp
+  wlddev2[names(temp)] <- temp
+  expect_identical(wlddev1, wlddev2)
+
+  wlddev1 <- wlddev
+  temp <- STD(get_vars(wlddev, 9:12))
+  add_vars(wlddev1, "front") <- temp
+  expect_identical(wlddev1, add_vars(temp, wlddev))
+
+  wlddev1 <- wlddev
+  temp <- STD(get_vars(wlddev, 9:12))
+  add_vars(wlddev1, c(10,12,14,16)) <- temp
+  expect_true(all_identical(wlddev1,  add_vars(wlddev, temp, pos = c(10,12,14,16)),
+                            add_vars(gv(wlddev, 1:9), gv(temp, 1), gv(wlddev, 10), gv(temp, 2),
+                                     gv(wlddev, 11), gv(temp, 3), gv(wlddev, 12), gv(temp, 4))))
+
+})
+
+test_that("replacing with or adding atomic elements works well", {
+  wlddev1 <- wlddev2 <- wlddev
+  get_vars(wlddev1, 9) <- wlddev$PCGDP
+  expect_identical(wlddev1, wlddev)
+
+  get_vars(wlddev1, 9) <- qM(wlddev[9:12])
+  wlddev2[9] <- qM(wlddev[9:12])
+  expect_identical(wlddev1, wlddev2)
+
+  wlddev1 <- wlddev2 <- wlddev
+  add_vars(wlddev1) <- wlddev$PCGDP
+  wlddev2["wlddev$PCGDP"] <- wlddev$PCGDP
+  expect_identical(wlddev1, wlddev2)
+
+  wlddev1 <- wlddev2 <- wlddev
+  add_vars(wlddev1) <- qM(wlddev[9:12])
+  wlddev2["qM(wlddev[9:12])"] <- qM(wlddev[9:12])
+  expect_identical(wlddev1, wlddev2)
+
+  wlddev1 <- wlddev2 <- wlddev
+  add_vars(wlddev1, "front") <- wlddev$PCGDP
+  expect_identical(wlddev1, add_vars(wlddev, wlddev$PCGDP, pos = 1))
+
+  wlddev1 <- wlddev2 <- wlddev
+  add_vars(wlddev1, "front") <- qM(wlddev[9:12])
+  expect_identical(wlddev1, add_vars(wlddev, qM(wlddev[9:12]), pos = 1))
+})
+
+
+
 test_that("select vars errors for wrong input", {
   expect_error(get_vars(wlddev, 13))
   expect_error(get_vars(wlddev, 1:13))
   expect_error(get_vars(wlddev, -13))
   expect_error(get_vars(wlddev, c("PCGDP","ODA3")))
+  expect_warning(get_vars(wlddev, "bla", regex = TRUE)) # Better give error
   expect_error(get_vars(wlddev, c(sapply(wlddev, is.numeric), TRUE)))
   expect_error(get_vars(wlddev, sapply(wlddev, is.numeric)[-1]))
 })
@@ -76,10 +138,30 @@ test_that("select vars errors for wrong input", {
 test_that("replace vars errors for wrong input", {
   expect_error(get_vars(wlddev, 13) <- wlddev[12])
   expect_error(get_vars(wlddev, "ODA3") <- wlddev[12])
+  expect_error(get_vars(wlddev, "bla", regex = TRUE) <- wlddev[12])
   expect_error(get_vars(wlddev, -13) <- wlddev[12])
   expect_error(get_vars(wlddev, 11:12) <- wlddev[12])
   expect_error(get_vars(wlddev, 9:12) <- wlddev[8:12])
   expect_invisible(get_vars(wlddev, 12) <- wlddev$ODA)
+  expect_error(get_vars(wlddev, 12) <- wlddev$ODA[-1])
+  expect_error(get_vars(wlddev, 12) <- qM(wlddev[9:12])[-1, ])
   expect_error(get_vars(wlddev, c(sapply(wlddev, is.numeric), TRUE)) <- wlddev)
   expect_error(get_vars(wlddev, sapply(wlddev, is.numeric)[-1]) <- wlddev)
 })
+
+test_that("add vars errors for wrong input", {
+  expect_error(add_vars(wlddev, 14) <- wlddev[12])
+  expect_error(add_vars(wlddev, "ODA3") <- wlddev[12])
+
+  expect_error(add_vars(wlddev) <- qM(wlddev[9:12])[-1, ])
+  expect_error(add_vars(wlddev, "front") <- qM(wlddev[9:12])[-1, ])
+  expect_error(add_vars(wlddev, 8) <- qM(wlddev[9:12])[-1, ])
+
+  expect_error(add_vars(wlddev) <- wlddev[-1, 9:12])
+  expect_error(add_vars(wlddev, "front") <- wlddev[-1, 9:12])
+  expect_error(add_vars(wlddev, 8) <- wlddev[-1, 9:12])
+
+  expect_error(add_vars(wlddev, 12) <- wlddev[9:12])
+  expect_error(add_vars(wlddev, 9:12) <- wlddev[9:10])
+})
+
