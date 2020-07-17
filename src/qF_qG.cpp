a7c93edd5ee7ce7992bbd3bd280269d04987d915
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-07-17 03:01:40 +0200

    Merging qFCpp and qGCpp, and fixing logical method.

--- src/qF_qG.cpp
+++ src/qF_qG.cpp
@@ -2,6 +2,107 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+
+template <int RTYPE>
+IntegerVector qFCppImpl(const Vector<RTYPE>& x, bool sort, bool ordered, bool na_exclude, int ret) {
+    Vector<RTYPE> levs = (sort && na_exclude) ? na_omit(sort_unique(x)) :
+                          sort ? sort_unique(x) :
+                          na_exclude ? na_omit(unique(x)) : unique(x);
+    IntegerVector out = (na_exclude || RTYPE != REALSXP) ? match(x, levs) : as<IntegerVector>(Rf_match(levs, x, NA_INTEGER));
+    if(ret == 1) { // returning a factor
+      SHALLOW_DUPLICATE_ATTRIB(out, x); // works for all atomic objects ?
+      if(RTYPE == STRSXP) {
+        out.attr("levels") = levs;
+      } else {
+        out.attr("levels") = Rf_coerceVector(levs, STRSXP); // What about date objects...
+      }
+      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
+                           ordered ? CharacterVector::create("ordered","factor") :
+                          (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
+    } else { // returnin a qG
+      out.attr("N.groups") = levs.size();
+      if(ret == 3) {
+        DUPLICATE_ATTRIB(levs, x);
+        out.attr("groups") = levs;
+      }
+      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
+        ordered ? CharacterVector::create("ordered","qG") :
+        (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
+    }
+    return out;
+}
+
+
+// [[Rcpp::export]]   // do Cpp 11 solution using return macro ?
+SEXP qFCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true, int ret = 1) {
+  switch(TYPEOF(x)) {
+  case INTSXP: return qFCppImpl<INTSXP>(x, sort, ordered, na_exclude, ret);
+  case REALSXP: return qFCppImpl<REALSXP>(x, sort, ordered, na_exclude, ret);
+  case STRSXP: return qFCppImpl<STRSXP>(x, sort, ordered, na_exclude, ret);
+  case LGLSXP: {
+    LogicalVector xl = x;
+    int l = xl.size();
+    LogicalVector nd(3);
+    IntegerVector out = no_init_vector(l);
+    if(na_exclude) {
+      for(int i = 0; i != l; ++i) {
+        if(xl[i] == NA_LOGICAL) {
+          out[i] = NA_INTEGER;
+        } else if(xl[i]) {
+          out[i] = 2;
+          nd[1] = true;
+        } else {
+          out[i] = 1;
+          nd[0] = true;
+        }
+      }
+      if(!nd[0]) for(int i = l; i--; ) if(out[i] == 2) out[i] = 1; // no FALSE // otherwise malformed factor.. only 2 level but not 1 level
+    } else {
+      for(int i = 0; i != l; ++i) {
+        if(xl[i] == NA_LOGICAL) {
+          out[i] = 3;
+          nd[2] = true;
+        } else if(xl[i]) {
+          out[i] = 2;
+          nd[1] = true;
+        } else {
+          out[i] = 1;
+          nd[0] = true;
+        }
+      }
+      if(!nd[0] || (nd[2] && !nd[1])) {
+        if(!nd[0]) { // no FALSE
+          if(nd[1]) { // has TRUE (and NA)
+            out = out - 1;
+          } else { // only has NA
+            out = out - 2;
+          }
+        } else { // NA and no TRUE
+          for(int i = l; i--; ) if(out[i] == 3) out[i] = 2;
+        }
+      }
+    }
+    if(ret == 1) { // return factor
+      SHALLOW_DUPLICATE_ATTRIB(out, x);
+      out.attr("levels") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
+      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
+                          ordered ? CharacterVector::create("ordered","factor") :
+                          (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
+    } else {
+      out.attr("N.groups") = int(nd[0]+nd[1]+nd[2]);
+      if(ret == 3) out.attr("groups") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
+      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
+                            ordered ? CharacterVector::create("ordered","qG") :
+                            (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
+    }
+    return out;
+  }
+  default: stop("Not Supported SEXP Type");
+  }
+  return R_NilValue;
+}
+
+
 // TODO: could still remove NA, and also for sort_unique
 template<int RTYPE>
 Vector<RTYPE> uniqueord(const Vector<RTYPE>& x) {
@@ -36,144 +137,9 @@ Vector<RTYPE> uniqueord(const Vector<RTYPE>& x) {
   return res;
 }
 
-template <int RTYPE>
-IntegerVector qFCppImpl(const Vector<RTYPE>& x, bool sort = true, bool ordered = true, bool na_exclude = true) {
-    Vector<RTYPE> levs = (sort && na_exclude) ? na_omit(sort_unique(x)) :
-                          sort ? sort_unique(x) :
-                          na_exclude ? na_omit(unique(x)) : unique(x);
-    IntegerVector out = (na_exclude || RTYPE != REALSXP) ? match(x, levs) : as<IntegerVector>(Rf_match(levs, x, NA_INTEGER));
-    SHALLOW_DUPLICATE_ATTRIB(out, x); // works for all atomic objects ?
-    if(RTYPE == STRSXP) {
-      out.attr("levels") = levs;
-    } else {
-      out.attr("levels") = Rf_coerceVector(levs, STRSXP); // What about date objects...
-    }
-    out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
-                         ordered ? CharacterVector::create("ordered","factor") :
-                        (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
-    return out;
-}
 
 template <int RTYPE>
-IntegerVector qGCppImpl(const Vector<RTYPE>& x, bool sort = true, bool ordered = true, bool na_exclude = true, bool retgrp = false) {
-  Vector<RTYPE> levs = (sort && na_exclude) ? na_omit(sort_unique(x)) :
-                        sort ? sort_unique(x) :
-                        na_exclude ? na_omit(unique(x)) : unique(x);
-  IntegerVector out = (na_exclude || RTYPE != REALSXP) ? match(x, levs) : as<IntegerVector>(Rf_match(levs, x, NA_INTEGER));
-  // SHALLOW_DUPLICATE_ATTRIB(out, x); // needed ? -> Nah, it's a programmers function..
-  out.attr("N.groups") = levs.size();
-  if(retgrp) {
-    DUPLICATE_ATTRIB(levs, x);
-    out.attr("groups") = levs;
-  }
-  out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
-                       ordered ? CharacterVector::create("ordered","qG") :
-                      (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
-  return out;
-}
-
-// [[Rcpp::export]]   // do Cpp 11 solution using return macro ?
-SEXP qFCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true) {
-  switch(TYPEOF(x)) {
-  case INTSXP: return qFCppImpl<INTSXP>(x, sort, ordered, na_exclude);
-  case REALSXP: return qFCppImpl<REALSXP>(x, sort, ordered, na_exclude);
-  case STRSXP: return qFCppImpl<STRSXP>(x, sort, ordered, na_exclude);
-  case LGLSXP: {
-    LogicalVector xl = x;
-    int l = xl.size();
-    LogicalVector nd(3);
-    IntegerVector out = no_init_vector(l);
-    if(na_exclude) {
-      for(int i = 0; i != l; ++i) {
-        if(xl[i] == NA_LOGICAL) {
-          out[i] = NA_INTEGER;
-        } else if(xl[i]) {
-          out[i] = 2;
-          nd[1] = true;
-        } else {
-          out[i] = 1;
-          nd[0] = true;
-        }
-      }
-    } else {
-      for(int i = 0; i != l; ++i) {
-        if(xl[i] == NA_LOGICAL) {
-          out[i] = 3;
-          nd[2] = true;
-        } else if(xl[i]) {
-          out[i] = 2;
-          nd[1] = true;
-        } else {
-          out[i] = 1;
-          nd[0] = true;
-        }
-      }
-    }
-    SHALLOW_DUPLICATE_ATTRIB(out, x);
-    out.attr("levels") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
-    out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
-                        ordered ? CharacterVector::create("ordered","factor") :
-                        (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
-    return out;
-  }
-  default: stop("Not Supported SEXP Type");
-  }
-  return R_NilValue;
-}
-
-// [[Rcpp::export]]   // do Cpp 11 solution using return macro ?
-SEXP qGCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true, bool retgrp = false) {
-  switch(TYPEOF(x)) {
-  case INTSXP: return qGCppImpl<INTSXP>(x, sort, ordered, na_exclude, retgrp);
-  case REALSXP: return qGCppImpl<REALSXP>(x, sort, ordered, na_exclude, retgrp);
-  case STRSXP: return qGCppImpl<STRSXP>(x, sort, ordered, na_exclude, retgrp);
-  case LGLSXP: {
-    LogicalVector xl = x;
-    int l = xl.size();
-    LogicalVector nd(3);
-    IntegerVector out = no_init_vector(l);
-    if(na_exclude) {
-      for(int i = 0; i != l; ++i) {
-        if(xl[i] == NA_LOGICAL) {
-          out[i] = NA_INTEGER;
-        } else if(xl[i]) {
-          out[i] = 2;
-          nd[1] = true;
-        } else {
-          out[i] = 1;
-          nd[0] = true;
-        }
-      }
-    } else {
-      for(int i = 0; i != l; ++i) {
-        if(xl[i] == NA_LOGICAL) {
-          out[i] = 3;
-          nd[2] = true;
-        } else if(xl[i]) {
-          out[i] = 2;
-          nd[1] = true;
-        } else {
-          out[i] = 1;
-          nd[0] = true;
-        }
-      }
-    }
-    // SHALLOW_DUPLICATE_ATTRIB(out, x);
-    out.attr("N.groups") = int(nd[0]+nd[1]+nd[2]);
-    if(retgrp) out.attr("groups") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
-    out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
-                         ordered ? CharacterVector::create("ordered","qG") :
-                        (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
-    return out;
-  }
-  default: stop("Not Supported SEXP Type");
-  }
-  return R_NilValue;
-}
-
-
-template <int RTYPE>
-Vector<RTYPE> funiqueImpl(const Vector<RTYPE>& x, bool sort = true) {
+Vector<RTYPE> funiqueImpl(const Vector<RTYPE>& x, bool sort) {
   if(sort) {
     Vector<RTYPE> out = sort_unique(x);
     DUPLICATE_ATTRIB(out, x);
