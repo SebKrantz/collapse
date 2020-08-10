eaef3d4202fe3b58f5a100a62611a5b4665b9fcf
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-08-10 05:20:49 +0200

    Moving auxiliary files to misc/ folder.

---
+++ misc/pkgdown_website.R
@@ -0,0 +1,17 @@
+# Make package website:
+# usethis::use_pkgdown() # overwrites _pkgdown.yml
+library(pkgdown)
+init_site()
+build_home()
+topics <- sort(setdiff(unlist(lapply(tools::Rd_db("collapse"),
+                                    tools:::.Rd_get_metadata, "name"), use.names = FALSE),
+                      c("collapse-package","collapse-documentation","A0-collapse-documentation","collapse-depreciated")))
+build_reference(examples = TRUE, topics = topics)
+build_articles(lazy = FALSE) # Still do with NCRAN = TRUE
+# Replace all collapse-documentation.html with index.html !!
+# Replce all <h1>Reference</h1> with <h1>Collapse Documentation & Overview</h1>
+# Replce all <h1>Articles</h1> with <h1>Vignettes / Articles</h1>
+build_news()
+preview_site()
+
+# ?build_home
