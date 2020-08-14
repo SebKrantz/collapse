79f9a4cb41366fa28cbf70f3bb9506c530be767e
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-08-14 18:51:00 +0200

    Fix example-building from collapse-package.Rd.

--- misc/pkgdown_website.R
+++ misc/pkgdown_website.R
@@ -3,16 +3,16 @@
 library(pkgdown)
 init_site()
 build_home(preview = FALSE)
+# in index.html, remove misc/figures.
 topics <- sort(setdiff(unlist(lapply(tools::Rd_db("collapse"),
                                     tools:::.Rd_get_metadata, "name"), use.names = FALSE),
                       c("collapse-documentation","A0-collapse-documentation","collapse-depreciated"))) # "collapse-package"
-build_reference(examples = TRUE, topics = topics)
+build_reference(examples = TRUE, topics = topics) # "collapse-package"
 # build_articles(lazy = FALSE) # Still do with NCRAN = TRUE
 # Replace all collapse-documentation.html with index.html !!
 # Replce all <h1>Reference</h1> with <h1>Documentation & Overview</h1>
 # Replce all <h1>Articles</h1> with <h1>Vignettes / Articles</h1>
 build_news()
-# in index.html, remove misc/figures.
 preview_site()
 
 # ?build_home
