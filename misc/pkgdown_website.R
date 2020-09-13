10d7ed49d08ad810e433791ba7b794a2b3aff266
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-09-13 06:48:09 +0200

    Updating website for v1.3.2.

--- misc/pkgdown_website.R
+++ misc/pkgdown_website.R
@@ -8,6 +8,7 @@ topics <- sort(setdiff(unlist(lapply(tools::Rd_db("collapse"),
                                     tools:::.Rd_get_metadata, "name"), use.names = FALSE),
                       c("collapse-documentation","A0-collapse-documentation","collapse-depreciated"))) # "collapse-package"
 build_reference(examples = TRUE, topics = topics) # "collapse-package"
+# Sys.setenv(NCRAN = "TRUE")
 # build_articles(lazy = FALSE) # Still do with NCRAN = TRUE
 # Replace all collapse-documentation.html with index.html !!
 # Replce all <h1>Reference</h1> with <h1>Documentation & Overview</h1>
