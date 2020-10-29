c8a80fd9e592d7b398126bd0a809e8d5b65f55f0
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-10-29 20:21:25 +0300

    loading lfe::demeanlist, RcppArmadillo::fastLmPure and RcppEigen::fastLmPure at package load.

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,41 @@
 
 .onLoad <- function(libname, pkgname) {
 
-  res <- .Call(C_collapse_init, "init.success")
+  res <- .Call(C_collapsedev2_init, "init.success")
   if(!is.character(res) || res != "init.success") stop("collapse not succesfully loaded!")
+
+  # https://stackoverflow.com/questions/12598242/global-variables-in-packages-in-r
+  # https://stackoverflow.com/questions/49056642/r-how-to-make-variable-available-to-namespace-at-loading-time?noredirect=1&lq=1
+  .collapse_env <- new.env()
+
+  suppressMessages({
+
+  .collapse_env$lfe_demeanlist <-
+         if(requireNamespace("lfe", quietly = TRUE)) # lfe::demeanlist else NULL
+             get0("demeanlist", envir = getNamespace("lfe")) else NULL
+
+  .collapse_env$RcppArmadillo_fastLm <-
+         if(requireNamespace("RcppArmadillo", quietly = TRUE)) # RcppArmadillo::fastLmPure else NULL
+           get0("fastLmPure", envir = getNamespace("RcppArmadillo")) else NULL # _RcppArmadillo_fastLm_impl
+
+  .collapse_env$RcppEigen_fastLm <-
+         if(requireNamespace("RcppEigen", quietly = TRUE)) # RcppEigen::fastLmPure else NULL
+           get0("fastLmPure", envir = getNamespace("RcppEigen")) else NULL # RcppEigen_fastLm_Impl
+
+  })
+
+  assign(".collapse_env", .collapse_env, envir = parent.env(environment()))
+
+  # Old solution: does not dynamically update, would have to re-install collapse after installing these packages
+  # assign(".lfe_demeanlist",
+  #        if(requireNamespace("lfe", quietly = TRUE))
+  #        get0("demeanlist", envir = getNamespace("lfe")) else NULL, envir = parent.env(environment()))
+  # assign(".RcppArmadillo_fastLm",
+  #        if(requireNamespace("RcppArmadillo", quietly = TRUE))
+  #        get0("_RcppArmadillo_fastLm_impl", envir = getNamespace("RcppArmadillo")) else NULL, envir = parent.env(environment()))
+  # assign(".RcppEigen_fastLm",
+  #        if(requireNamespace("RcppEigen", quietly = TRUE))
+  #        get0("RcppEigen_fastLm_Impl", envir = getNamespace("RcppEigen")) else NULL, envir = parent.env(environment()))
 
   options("collapse_unused_arg_action" = "warning") # error, warning, message or none
 
@@ -10,12 +43,15 @@
 }
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("collapse ",packageVersion("collapse"),", see ?`collapse-package` or ?`collapse-documentation`\nNote: stats::D  ->  D.expression, D.call, D.name"))
+  packageStartupMessage(paste0("collapsedev2 ",packageVersion("collapsedev2"),", see ?`collapse-package` or ?`collapse-documentation`\nNote: stats::D  ->  D.expression, D.call, D.name"))
 }
 
 .onUnload <- function (libpath) {
-  library.dynam.unload("collapse", libpath)
+  library.dynam.unload("collapsedev2", libpath)
 }
+
+# Note: To create local dev version of package change package name in DESCRIPTION, NAMESPACE, this file,
+# replace all instances of `_collapse_` in source files, and also rename `R_init_collapse` in ExportSymbols.cpp.
 
 release_questions <- function() {
   c(
