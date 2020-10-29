d1bf75a0e15d788ad711547a3930e9d6bc7eb5e2
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-10-29 20:33:58 +0300

    Added factor methods, fdroplevels + improved attribute handling.

--- src/qF_qG.cpp
+++ src/qF_qG.cpp
@@ -4,41 +4,41 @@ using namespace Rcpp;
 
 
 template <int RTYPE>
-IntegerVector qFCppImpl(const Vector<RTYPE>& x, bool sort, bool ordered, bool na_exclude, int ret) {
+IntegerVector qFCppImpl(const Vector<RTYPE>& x, bool sort, bool ordered, bool na_exclude, bool keep_attr, int ret) {
     Vector<RTYPE> levs = (sort && na_exclude) ? na_omit(sort_unique(x)) :
                           sort ? sort_unique(x) :
                           na_exclude ? na_omit(unique(x)) : unique(x);
     IntegerVector out = (na_exclude || RTYPE != REALSXP) ? match(x, levs) : as<IntegerVector>(Rf_match(levs, x, NA_INTEGER));
     if(ret == 1) { // returning a factor
-      SHALLOW_DUPLICATE_ATTRIB(out, x); // works for all atomic objects ?
+      if(keep_attr) SHALLOW_DUPLICATE_ATTRIB(out, x); // works for all atomic objects ?
       if(RTYPE == STRSXP) {
-        out.attr("levels") = levs;
+        Rf_setAttrib(out, R_LevelsSymbol, levs);
       } else {
-        out.attr("levels") = Rf_coerceVector(levs, STRSXP); // What about date objects...
+        Rf_setAttrib(out, R_LevelsSymbol, Rf_coerceVector(levs, STRSXP)); // What about date objects...
       }
-      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
+      Rf_classgets(out, (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
                            ordered ? CharacterVector::create("ordered","factor") :
-                          (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
+                          (!na_exclude) ? CharacterVector::create("factor","na.included") : CharacterVector::create("factor"));
     } else { // returnin a qG
       out.attr("N.groups") = levs.size();
       if(ret == 3) {
         DUPLICATE_ATTRIB(levs, x);
         out.attr("groups") = levs;
       }
-      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
+      Rf_classgets(out, (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
         ordered ? CharacterVector::create("ordered","qG") :
-        (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
+        (!na_exclude) ? CharacterVector::create("qG","na.included") : CharacterVector::create("qG"));
     }
     return out;
 }
 
 
 // [[Rcpp::export]]   // do Cpp 11 solution using return macro ?
-SEXP qFCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true, int ret = 1) {
+SEXP qFCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true, bool keep_attr = true, int ret = 1) {
   switch(TYPEOF(x)) {
-  case INTSXP: return qFCppImpl<INTSXP>(x, sort, ordered, na_exclude, ret);
-  case REALSXP: return qFCppImpl<REALSXP>(x, sort, ordered, na_exclude, ret);
-  case STRSXP: return qFCppImpl<STRSXP>(x, sort, ordered, na_exclude, ret);
+  case INTSXP: return qFCppImpl<INTSXP>(x, sort, ordered, na_exclude, keep_attr, ret);
+  case REALSXP: return qFCppImpl<REALSXP>(x, sort, ordered, na_exclude, keep_attr, ret);
+  case STRSXP: return qFCppImpl<STRSXP>(x, sort, ordered, na_exclude, keep_attr, ret);
   case LGLSXP: {
     LogicalVector xl = x;
     int l = xl.size();
@@ -83,17 +83,21 @@ SEXP qFCpp(SEXP x, bool sort = true, bool ordered = true, bool na_exclude = true
       }
     }
     if(ret == 1) { // return factor
-      SHALLOW_DUPLICATE_ATTRIB(out, x);
-      out.attr("levels") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
-      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
+      if(keep_attr) SHALLOW_DUPLICATE_ATTRIB(out, x);
+      Rf_setAttrib(out, R_LevelsSymbol, CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd]);
+      Rf_classgets(out, (ordered && !na_exclude) ? CharacterVector::create("ordered","factor","na.included") :
                           ordered ? CharacterVector::create("ordered","factor") :
-                          (!na_exclude) ? CharacterVector::create("factor","na.included") : "factor";
+                          (!na_exclude) ? CharacterVector::create("factor","na.included") : CharacterVector::create("factor"));
     } else {
       out.attr("N.groups") = int(nd[0]+nd[1]+nd[2]);
-      if(ret == 3) out.attr("groups") = CharacterVector::create("FALSE", "TRUE", NA_STRING)[nd];
-      out.attr("class") = (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
+      if(ret == 3) {
+        LogicalVector groups = LogicalVector::create(false, true, NA_LOGICAL)[nd];
+        DUPLICATE_ATTRIB(groups, x);
+        out.attr("groups") = groups;
+      }
+      Rf_classgets(out, (ordered && !na_exclude) ? CharacterVector::create("ordered","qG","na.included") :
                             ordered ? CharacterVector::create("ordered","qG") :
-                            (!na_exclude) ? CharacterVector::create("qG","na.included") : "qG";
+                            (!na_exclude) ? CharacterVector::create("qG","na.included") : CharacterVector::create("qG"));
     }
     return out;
   }
@@ -153,10 +157,64 @@ Vector<RTYPE> funiqueImpl(const Vector<RTYPE>& x, bool sort) {
   }
 }
 
+
+IntegerVector funiqueFACT(const IntegerVector& x, bool sort = true) {
+  int nlevp = Rf_nlevels(x)+1, l = x.size(), k = 0;
+  std::vector<bool> not_seen(nlevp, true);
+  bool countNA = true;
+  if(sort) {
+    for(int i = 0; i != l; ++i) {
+      if(x[i] == NA_INTEGER) {
+        if(countNA) {
+          ++k;
+          countNA = false;
+        }
+        continue;
+      }
+      if(not_seen[x[i]]) {
+        not_seen[x[i]] = false;
+        if(++k == nlevp) break;
+      }
+    }
+    IntegerVector out = no_init_vector(k);
+    if(!countNA) out[k-1] = NA_INTEGER;
+    k = 0;
+    for(int i = 1; i != nlevp; ++i) if(!not_seen[i]) out[k++] = i;
+    DUPLICATE_ATTRIB(out, x);
+    return out;
+  } else {
+    IntegerVector uxp = no_init_vector(nlevp);
+    for(int i = 0; i != l; ++i) {
+      if(x[i] == NA_INTEGER) {
+        if(countNA) {
+          uxp[k++] = NA_INTEGER;
+          countNA = false;
+        }
+        continue;
+      }
+      if(not_seen[x[i]]) {
+        uxp[k++] = x[i];
+        if(k == nlevp) {
+          DUPLICATE_ATTRIB(uxp, x);
+          return uxp;
+        }
+        not_seen[x[i]] = false;
+      }
+    }
+    IntegerVector out = no_init_vector(k);
+    for(int i = 0; i != k; ++i) out[i] = uxp[i];
+    DUPLICATE_ATTRIB(out, x);
+    return out;
+  }
+}
+
+
 // [[Rcpp::export]]
 SEXP funiqueCpp(SEXP x, bool sort = true) {
   switch(TYPEOF(x)) {
-  case INTSXP: return funiqueImpl<INTSXP>(x, sort);
+  case INTSXP:
+    if(Rf_isFactor(x)) return funiqueFACT(x, sort);
+    return funiqueImpl<INTSXP>(x, sort);
   case REALSXP: return funiqueImpl<REALSXP>(x, sort);
   case STRSXP: return funiqueImpl<STRSXP>(x, sort);
   case LGLSXP: {
@@ -186,3 +244,55 @@ SEXP funiqueCpp(SEXP x, bool sort = true) {
   return R_NilValue;
 }
 
+
+
+// [[Rcpp::export]]
+IntegerVector fdroplevelsCpp(const IntegerVector& x, bool check_NA = true) {
+
+  int nlevp = Rf_nlevels(x)+1, l = x.size(), n = 1;
+  std::vector<int> uxp(nlevp, 1); // 1 is also true !
+  bool anyNA = false;
+  if(check_NA) {
+    for(int i = 0; i != l; ++i) {
+      if(x[i] == NA_INTEGER) {
+        anyNA = true;
+        continue;
+      }
+      if(uxp[x[i]]) {
+        uxp[x[i]] = 0;
+        if(++n == nlevp) return x;
+      }
+      // uxp[x[i]] = 1; // Runs through, slower than above on DHS Uganda (660 factors)
+    }
+  } else {
+    for(int i = 0; i != l; ++i) {
+      if(uxp[x[i]]) {
+        uxp[x[i]] = 0;
+        if(++n == nlevp) return x;
+      }
+      // uxp[x[i]] = 1; // Runs through, slower than above on DHS Uganda (660 factors)
+    }
+  }
+  // n = std::accumulate(uxp.begin()+1, uxp.end(), 0);
+  // if(n == nlevp-1) return x;
+  CharacterVector levs = Rf_getAttrib(x, R_LevelsSymbol);
+  CharacterVector newlevs = no_init_vector(n-1); // n
+  n = 0;
+  for(int i = 1; i != nlevp; ++i) {
+    if(!uxp[i]) {
+      newlevs[n] = levs[i-1];
+      uxp[i] = ++n;
+    }
+  }
+  IntegerVector out = no_init_vector(l); // fastest solution !
+  // IntegerVector out = anyNA ? IntegerVector(l, NA_INTEGER) : no_init_vector(l); // Not faster !!
+  if(anyNA) {
+    // for(int i = 0; i != l; ++i) if(x[i] != NA_INTEGER) out[i] = uxp[x[i]];
+    for(int i = 0; i != l; ++i) out[i] = (x[i] == NA_INTEGER) ? NA_INTEGER : uxp[x[i]];
+  } else {
+    for(int i = 0; i != l; ++i) out[i] = uxp[x[i]];
+  }
+  DUPLICATE_ATTRIB(out, x);
+  Rf_setAttrib(out, R_LevelsSymbol, newlevs);
+  return out;
+}
