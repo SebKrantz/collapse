b8a25dfe29b173126e7b45a1b20ccb59c7c4804c
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-10-30 23:34:56 +0300

    Expanded test coverage.

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -4,13 +4,24 @@ context("Misc")
 
 m <- na_insert(qM(mtcars))
 
-test_that("descr, pwcor, cov, Nobs, misc", {
+test_that("descr, pwcor, pwcov, pwNobs", {
   expect_visible(descr(wlddev))
+  expect_visible(as.data.frame(descr(wlddev)))
+  expect_output(print(descr(wlddev)))
   expect_visible(descr(GGDC10S))
-  expect_visible(pwcor(nv(wlddev)))
+  expect_output(print(pwcor(nv(wlddev))))
+  expect_output(print(pwcor(nv(wlddev), N = TRUE)))
+  expect_output(print(pwcor(nv(wlddev), P = TRUE)))
+  expect_output(print(pwcor(nv(wlddev), N = TRUE, P = TRUE)))
+  expect_output(print(pwcor(nv(wlddev), N = TRUE, P = TRUE, use = "complete.obs")))
   expect_visible(pwcor(nv(GGDC10S)))
   expect_visible(pwcov(nv(wlddev)))
-  expect_visible(pwcov(nv(GGDC10S)))
+  expect_output(print(pwcov(nv(wlddev))))
+  expect_output(print(pwcov(nv(wlddev), N = TRUE)))
+  expect_output(print(pwcov(nv(wlddev), P = TRUE)))
+  expect_output(print(pwcov(nv(wlddev), N = TRUE, P = TRUE)))
+  expect_output(print(pwcov(nv(wlddev), N = TRUE, P = TRUE, use = "complete.obs")))
+
   expect_visible(pwNobs(wlddev))
   expect_visible(pwNobs(GGDC10S))
 
@@ -18,5 +29,23 @@ test_that("descr, pwcor, cov, Nobs, misc", {
   expect_visible(pwcor(m))
   expect_visible(pwcov(m))
   expect_visible(pwNobs(m))
+
+})
+
+test_that("deep matrix dispatch works well", {
+  tsm <- EuStockMarkets
+  class(tsm) <- setdiff(class(tsm), "matrix")
+  f <- qF(sample.int(5, nrow(tsm), TRUE))
+  NCOL2 <- function(x) if(length(d <- dim(x)) > 1L) d[2L] else length(x)
+
+  for(i in setdiff(c(.FAST_FUN, .OPERATOR_FUN), c("fnth","flag","L","F", "fdiff","D","Dlog", "fgrowth","G")))
+      expect_equal(NCOL2(match.fun(i)(tsm, f)), 4L)
+
+  expect_equal(NCOL2(fnth(tsm, 0.5, f)), 4L)
+  expect_equal(NCOL2(BY(tsm, f, sum)), 4L)
+  expect_equal(nrow(qsu(tsm)), 4L)
+
+  for(i in c("flag", "L", "fdiff", "D", "Dlog", "fgrowth", "G"))
+      expect_true(all(is.na(match.fun(i)(tsm)[1L, ])))
 
 })
--- tests/testthat/test-roworder-colorder-rename.R
+++ tests/testthat/test-roworder-colorder-rename.R
@@ -1,4 +1,4 @@
-context("roworder, colorder, rename")
+context("roworder, colorder, frename")
 
 test_that("roworder works as intended", {
 
@@ -14,5 +14,59 @@ test_that("roworder works as intended", {
   expect_identical(roworderv(mtcars, neworder = 3:4, pos = "end"), rbind(mtcars[-(3:4), ], mtcars[3:4, ]))  # Bring them to the end
   expect_identical(roworderv(mtcars, neworder = mtcars$vs == 1), rbind(mtcars[mtcars$vs == 1, ], mtcars[mtcars$vs != 1, ]))    # Bring rows with vs == 1 to the top
   expect_identical(ss(roworderv(mtcars, neworder = c(8, 2), pos = "exchange"), c(2,8)), ss(mtcars, c(8,2)))
+
+})
+
+library(magrittr)
+test_that("colorder works as intended", {
+  expect_identical(colorder(mtcars, vs, cyl:hp, am),
+                   fselect(mtcars, vs, cyl:hp, am, return = "indices") %>% {cbind(mtcars[.], mtcars[-.])})
+  expect_identical(colorder(mtcars, vs, cyl:hp, am, pos = "end"),
+                   fselect(mtcars, vs, cyl:hp, am, return = "indices") %>% {cbind(mtcars[-.], mtcars[.])})
+  expect_identical(colorder(mtcars, vs, cyl:hp, am, pos = "exchange"),
+                   fselect(mtcars, vs, cyl:hp, am, return = "indices") %>% {`get_vars<-`(mtcars, sort(.), value = mtcars[.])})
+
+  ## Same in standard evaluation
+  expect_identical(colorder(mtcars, vs, cyl:hp, am),
+                   colorderv(mtcars, c(8, 2:4, 9)))
+  expect_identical(colorder(mtcars, vs, cyl:hp, am, pos = "end"),
+                   colorderv(mtcars, c(8, 2:4, 9), pos = "end"))
+  expect_identical(colorder(mtcars, vs, cyl:hp, am, pos = "exchange"),
+                   colorderv(mtcars, c(8, 2:4, 9), pos = "exchange"))
+
+  expect_identical(colorder(mtcars, vs, cyl, am),
+                   colorderv(mtcars, c("vs", "cyl|am"), regex = TRUE))
+
+})
+
+test_that("frename works as intended", {
+
+  ## Using tagged expressions
+  expect_equal(frename(iris, Sepal.Length = SL, Sepal.Width = SW,
+                       Petal.Length = PL, Petal.Width = PW), setNames(iris, .c(SL, SW, PL, PW, Species)))
+
+  expect_equal(frename(iris, Sepal.Length = "S L", Sepal.Width = "S W",
+                       Petal.Length = "P L", Petal.Width = "P W"), setNames(iris, c("S L", "S W", "P L", "P W", "Species")))
+
+  ## Using a function
+  expect_equal(frename(iris, tolower), setNames(iris, tolower(names(iris))))
+  expect_equal(frename(iris, tolower, cols = 1:2), setNames(iris, c(tolower(names(iris)[1:2]), names(iris)[-(1:2)])))
+  expect_equal(frename(iris, tolower, cols = is.numeric), setNames(iris, c(tolower(names(iris)[1:4]), names(iris)[-(1:4)])))
+  expect_equal(frename(iris, paste, "new", sep = "_", cols = 1:2), setNames(iris, c(paste(names(iris)[1:2], "new", sep = "_"), names(iris)[-(1:2)])))
+
+  ## Renaming by reference
+  iris2 <- iris
+  setrename(iris2, tolower)
+  expect_equal(iris2, setNames(iris, tolower(names(iris))))
+  iris2 <- iris
+  setrename(iris2, tolower, cols = 1:2)
+  expect_equal(iris2, setNames(iris, c(tolower(names(iris)[1:2]), names(iris)[-(1:2)])))
+  iris2 <- iris
+  setrename(iris2, tolower, cols = is.numeric)
+  expect_equal(iris2, setNames(iris, c(tolower(names(iris)[1:4]), names(iris)[-(1:4)])))
+  iris2 <- iris
+  setrename(iris2, paste, "new", sep = "_", cols = 1:2)
+  expect_equal(iris2, setNames(iris, c(paste(names(iris)[1:2], "new", sep = "_"), names(iris)[-(1:2)])))
+  rm(iris2)
 
 })
