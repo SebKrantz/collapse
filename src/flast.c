e08aaaddce92416afe73fdeb81581376e9a2b422
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2021-06-16 21:12:34 +0300

    Final tweaks to ffirst and flast (in C) and separating files.

--- src/flast.c
+++ src/flast.c
@@ -1,45 +1,37 @@
 #include "collapse_c.h"
-// #include <stdint.h>
-// #include <stdbool.h>
-
-// TODO: Implemented smarter copy names ?!
-// About Pointers
-// https://www.tutorialspoint.com/cprogramming/c_pointers.htm
-// https://www.tutorialspoint.com/cprogramming/c_pointer_arithmetic.htm
 
 
-// Use const ?
-SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
+SEXP flast_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
 
-  int l = length(x), tx = TYPEOF(x), end = l-1;
+  int l = length(x), tx = TYPEOF(x);
   if (l < 2) return x; // Prevents seqfault for numeric(0) #101
   if (ng == 0) {
     SEXP out = PROTECT(allocVector(tx, 1));
-    int j = 0;
+    int j = l-1;
     if(narm) {
       switch(tx) {
         case REALSXP: {
           double *px = REAL(x);
-          while(ISNAN(px[j]) && j != end) ++j;
+          while(ISNAN(px[j]) && j != 0) --j;
           REAL(out)[0] = px[j];
           break;
         }
         case STRSXP: {
           SEXP *px = STRING_PTR(x);
-          while(px[j] == NA_STRING && j != end) ++j;
+          while(px[j] == NA_STRING && j != 0) --j;
           SET_STRING_ELT(out, 0, px[j]);
           break;
         }
         case INTSXP:
         case LGLSXP: {
           int *px = INTEGER(x);
-          while(px[j] == NA_INTEGER && j != end) ++j;
+          while(px[j] == NA_INTEGER && j != 0) --j;
           INTEGER(out)[0] = px[j];
           break;
         }
         case VECSXP: {
           SEXP *px = SEXPPTR(x);
-          while(length(px[j]) == 0 && j != end) ++j;
+          while(length(px[j]) == 0 && j != 0) --j;
           SET_VECTOR_ELT(out, 0, px[j]);
           break;
         }
@@ -47,16 +39,16 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
       }
     } else {
       switch(tx) {
-      case REALSXP: REAL(out)[0] = REAL(x)[0];
-        break;
-      case STRSXP: SET_STRING_ELT(out, 0, STRING_ELT(x, 0));
-        break;
-      case INTSXP:
-      case LGLSXP: INTEGER(out)[0] = INTEGER(x)[0];
-        break;
-      case VECSXP: SET_VECTOR_ELT(out, 0, VECTOR_ELT(x, 0));
-        break;
-      default: error("Unsupported SEXP type!");
+        case REALSXP: REAL(out)[0] = REAL(x)[l-1];
+          break;
+        case STRSXP: SET_STRING_ELT(out, 0, STRING_ELT(x, l-1));
+          break;
+        case INTSXP:
+        case LGLSXP: INTEGER(out)[0] = INTEGER(x)[l-1];
+          break;
+        case VECSXP: SET_VECTOR_ELT(out, 0, VECTOR_ELT(x, l-1));
+          break;
+        default: error("Unsupported SEXP type!");
       }
     }
     copyMostAttrib(x, out);
@@ -74,8 +66,8 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
         double *px = REAL(x), *pout = REAL(out);
         for(int i = ng; i--; ) pout[i] = NA_REAL;
         --pout;
-        for(int i = 0; i != l; ++i) {
-          if(NISNAN(px[i])) { // Fastest ???
+        for(int i = l; i--; ) {
+          if(NISNAN(px[i])) {
             if(ISNAN(pout[pg[i]])) {
               pout[pg[i]] = px[i];
               if(++ngs == ng) break;
@@ -88,7 +80,7 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
         SEXP *px = STRING_PTR(x), *pout = STRING_PTR(out);
         for(int i = ng; i--; ) pout[i] = NA_STRING;
         --pout;
-        for(int i = 0; i != l; ++i) {
+        for(int i = l; i--; ) {
           if(px[i] != NA_STRING) {
             if(pout[pg[i]] == NA_STRING) {
               pout[pg[i]] = px[i];
@@ -103,7 +95,7 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
         int *px = INTEGER(x), *pout = INTEGER(out);
         for(int i = ng; i--; ) pout[i] = NA_INTEGER;
         --pout;
-        for(int i = 0; i != l; ++i) {
+        for(int i = l; i--; ) {
           if(px[i] != NA_INTEGER) {
             if(pout[pg[i]] == NA_INTEGER) {
               pout[pg[i]] = px[i];
@@ -115,9 +107,9 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
       }
       case VECSXP: {
         SEXP *px = SEXPPTR(x), *pout = SEXPPTR(out);
-        for(int i = ng; i--; ) pout[i] = R_NilValue; // R_NilValue or just leave empty ??
+        for(int i = ng; i--; ) pout[i] = R_NilValue;
         --pout;
-        for(int i = 0; i != l; ++i) {
+        for(int i = l; i--; ) {
           if(length(px[i])) {
             if(pout[pg[i]] == R_NilValue) {
               pout[pg[i]] = px[i];
@@ -130,17 +122,6 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
       default: error("Unsupported SEXP type!");
       }
     } else {
-      // Old Implementation: With boolean array
-      //      bool gl[ng+1];
-      //      memset(gl, 1, sizeof(bool) * (ng+1));
-      //        for(int i = 0; i != l; ++i) {
-      //          if(gl[pg[i]]) {
-      //            gl[pg[i]] = false;
-      //            pout[pg[i]] = px[i];
-      //            ++ngs;
-      //            if(ngs == ng) break;
-      //          }
-      //        }
       switch(tx) {
       case REALSXP: {
         double *px = REAL(x), *pout = REAL(out);
@@ -162,70 +143,51 @@ SEXP ffirst_impl(SEXP x, int ng, SEXP g, int narm, int *gl) {
       default: error("Unsupported SEXP type!");
       }
     }
-    copyMostAttrib(x, out); // DUPLICATE_ATTRIB(out, x);
+    copyMostAttrib(x, out);
     UNPROTECT(1);
     return out;
   }
 }
 
-SEXP ffirstC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm) {
+SEXP flastC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm) {
   int *pgl, ng = asInteger(Rng), narm = asInteger(Rnarm);
   if(ng == 0 || narm) {
-    pgl = &ng; // TO avoid Wmaybe uninitialized
-    return ffirst_impl(x, ng, g, narm, pgl);
+    pgl = &ng;
+    return flast_impl(x, ng, g, narm, pgl);
   }
-
-  // Using C-Array -> Error, possibly because unprotected ???
-  int gl[ng], *pg = INTEGER(g), lg = length(g);
+  int gl[ng], *pg = INTEGER(g);
   pgl = &gl[0];
   for(int i = ng; i--; ) pgl[i] = NA_INTEGER;
-  --pgl; // &gl[0]-1 Or gl-1; // Pointer to -1 array element (since g starts from 1): https://beginnersbook.com/2014/01/c-pointer-to-array-example/
-  for(int i = 0; i != lg; ++i) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i; // Correct? even for first value ?
-
-  //  SEXP gl = PROTECT(allocVector(INTSXP, ng));
-  //  memset(gl, 0, sizeof(int)*ng); //
-  //  int *pg = INTEGER(g);
-  //  pgl = INTEGER(gl)-1; // Pointer to -1 array element (since g starts from 1): https://beginnersbook.com/2014/01/c-pointer-to-array-example/
-  //  for(int i = length(g); i--; ) if(!pgl[pg[i]]) pgl[pg[i]] = i; // Correct? even for first value ?
-
-
-  // SEXP out = PROTECT(allocVector(INTSXP, ng));
-  // int *pout = INTEGER(out);
-  // for(int i = ng; i--; ) pout[i] = pgl[i+1];
-  // UNPROTECT(1);
-  // return out; // Checking pointer: appears to be correct...
-  // UNPROTECT(1);
-  // return gl;
-  return ffirst_impl(x, ng, g, narm, ++pgl);
+  --pgl;
+  for(int i = length(g); i--; ) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i;
+  return flast_impl(x, ng, g, narm, ++pgl);
 }
 
-SEXP ffirstlC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm) {
+SEXP flastlC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm) {
   int l = length(x), *pgl, ng = asInteger(Rng), narm = asInteger(Rnarm), nprotect = 1;
   if(ng > 0 && !narm) {
-    // Cant use integer array here because apparently it is removed by the garbage collector when passed to a new function
     SEXP gl = PROTECT(allocVector(INTSXP, ng)); ++nprotect;
-    int *pg = INTEGER(g), lg = length(g); // gl[ng],
-    pgl = INTEGER(gl); // pgl = &gl[0];
+    int *pg = INTEGER(g);
+    pgl = INTEGER(gl);
     for(int i = ng; i--; ) pgl[i] = NA_INTEGER;
     --pgl;
-    for(int i = 0; i != lg; ++i) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i;
+    for(int i = length(g); i--; ) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i;
     ++pgl;
-  } else pgl = &l; // To avoid Wmaybe uninitialized..
-  // return ffirst_impl(VECTOR_ELT(x, 0), ng, g, narm, pgl);
+  } else pgl = &l;
   SEXP out = PROTECT(allocVector(VECSXP, l));
   SEXP *px = SEXPPTR(x), *pout = SEXPPTR(out);
-  for(int j = 0; j != l; ++j) pout[j] = ffirst_impl(px[j], ng, g, narm, pgl);
+  for(int j = 0; j != l; ++j) pout[j] = flast_impl(px[j], ng, g, narm, pgl);
   DFcopyAttr(out, x, ng);
   UNPROTECT(nprotect);
   return out;
 }
 
 // For matrix writing a separate function to increase efficiency.
-SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
+SEXP flastmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
   SEXP dim = getAttrib(x, R_DimSymbol);
   if(isNull(dim)) error("x is not a matrix");
   int tx = TYPEOF(x), ng = asInteger(Rng), narm = asInteger(Rnarm),
-    l = INTEGER(dim)[0], col = INTEGER(dim)[1], end = l-1;
+    l = INTEGER(dim)[0], col = INTEGER(dim)[1];
   if (l < 2) return x;
   if (ng == 0) {
     SEXP out = PROTECT(allocVector(tx, col));
@@ -233,34 +195,34 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
       switch(tx) {
       case REALSXP: {
         double *px = REAL(x), *pout = REAL(out);
-        for(int j = 0, i = 0; j != col; ++j) {
-          while(ISNAN(px[i]) && i != end) ++i;
-          pout[j] = px[i]; px += l; i = 0;
+        for(int j = 0, i = l-1; j != col; ++j) {
+          while(ISNAN(px[i]) && i != 0) --i;
+          pout[j] = px[i]; px += l; i = l-1;
         }
         break;
       }
       case STRSXP: {
         SEXP *px = STRING_PTR(x), *pout = STRING_PTR(out);
-        for(int j = 0, i = 0; j != col; ++j) {
-          while(px[i] == NA_STRING && i != end) ++i;
-          pout[j] = px[i]; px += l; i = 0;
+        for(int j = 0, i = l-1; j != col; ++j) {
+          while(px[i] == NA_STRING && i != 0) --i;
+          pout[j] = px[i]; px += l; i = l-1;
         }
         break;
       }
       case INTSXP:
       case LGLSXP: {
         int *px = INTEGER(x), *pout = INTEGER(out);
-        for(int j = 0, i = 0; j != col; ++j) {
-          while(px[i] == NA_INTEGER && i != end) ++i;
-          pout[j] = px[i]; px += l; i = 0;
+        for(int j = 0, i = l-1; j != col; ++j) {
+          while(px[i] == NA_INTEGER && i != 0) --i;
+          pout[j] = px[i]; px += l; i = l-1;
         }
         break;
       }
       case VECSXP: {
-        SEXP *px = SEXPPTR(x), *pout = SEXPPTR(out);
-        for(int j = 0, i = 0; j != col; ++j) {
-          while(length(px[i]) == 0 && i != end) ++i;
-          pout[j] = px[i]; px += l; i = 0;
+        SEXP *px = STRING_PTR(x), *pout = STRING_PTR(out);
+        for(int j = 0, i = l-1; j != col; ++j) {
+          while(length(px[i]) == 0 && i != 0) --i;
+          pout[j] = px[i]; px += l; i = l-1;
         }
         break;
       }
@@ -270,19 +232,19 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
       switch(tx) {
       case REALSXP: {
         double *px = REAL(x), *pout = REAL(out);
-        for(int j = 0; j != col; ++j) pout[j] = px[j * l];
+        for(int j = 0; j != col; ++j) pout[j] = px[j * l + l-1];
         break;
       }
       case INTSXP:
       case LGLSXP: {
         int *px = INTEGER(x), *pout = INTEGER(out);
-        for(int j = 0; j != col; ++j) pout[j] = px[j * l];
+        for(int j = 0; j != col; ++j) pout[j] = px[j * l + l-1];
         break;
       }
       case STRSXP:
       case VECSXP: {
         SEXP *px = SEXPPTR(x), *pout = SEXPPTR(out);
-        for(int j = 0; j != col; ++j) pout[j] = px[j * l];
+        for(int j = 0; j != col; ++j) pout[j] = px[j * l + l-1];
         break;
       }
       default: error("Unsupported SEXP type!");
@@ -302,7 +264,7 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
         for(int i = ng * col; i--; ) pout[i] = NA_REAL;
         --pout;
         for(int j = 0; j != col; ++j) {
-          for(int i = 0; i != l; ++i) if(NISNAN(px[i]) && ISNAN(pout[pg[i]])) pout[pg[i]] = px[i];
+          for(int i = l; i--; ) if(NISNAN(px[i]) && ISNAN(pout[pg[i]])) pout[pg[i]] = px[i];
           px += l; pout += ng;
         }
         break;
@@ -312,7 +274,7 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
         for(int i = ng * col; i--; ) pout[i] = NA_STRING;
         --pout;
         for(int j = 0; j != col; ++j) {
-          for(int i = 0; i != l; ++i) if(px[i] != NA_STRING && pout[pg[i]] == NA_STRING) pout[pg[i]] = px[i];
+          for(int i = l; i--; ) if(px[i] != NA_STRING && pout[pg[i]] == NA_STRING) pout[pg[i]] = px[i];
           px += l; pout += ng;
         }
         break;
@@ -323,7 +285,7 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
         for(int i = ng * col; i--; ) pout[i] = NA_INTEGER;
         --pout;
         for(int j = 0; j != col; ++j) {
-          for(int i = 0; i != l; ++i) if(px[i] != NA_INTEGER && pout[pg[i]] == NA_INTEGER) pout[pg[i]] = px[i];
+          for(int i = l; i--; ) if(px[i] != NA_INTEGER && pout[pg[i]] == NA_INTEGER) pout[pg[i]] = px[i];
           px += l; pout += ng;
         }
         break;
@@ -333,7 +295,7 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
         for(int i = ng * col; i--; ) pout[i] = R_NilValue;
         --pout;
         for(int j = 0; j != col; ++j) {
-          for(int i = 0; i != l; ++i) if(length(px[i]) && pout[pg[i]] == R_NilValue) pout[pg[i]] = px[i];
+          for(int i = l; i--; ) if(length(px[i]) && pout[pg[i]] != R_NilValue) pout[pg[i]] = px[i];
           px += l; pout += ng;
         }
         break;
@@ -345,7 +307,7 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
       pgl = &gl[0];
       for(int i = ng; i--; ) pgl[i] = NA_INTEGER;
       --pgl;
-      for(int i = 0; i != l; ++i) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i;
+      for(int i = l; i--; ) if(pgl[pg[i]] == NA_INTEGER) pgl[pg[i]] = i;
       ++pgl;
       switch(tx) {
       case REALSXP: {
@@ -382,20 +344,3 @@ SEXP ffirstmC(SEXP x, SEXP Rng, SEXP g, SEXP Rnarm, SEXP Rdrop) {
     return out;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
