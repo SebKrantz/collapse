551e4eff2fac977059e6ba915899f0a0b3a8e436
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2021-06-16 23:24:16 +0300

    Faster ISNAN.

--- src/collapse_c.h
+++ src/collapse_c.h
@@ -3,6 +3,9 @@
 
 #define SEXPPTR(x) ((SEXP *)DATAPTR(x))  // to avoid overhead of looped VECTOR_ELT
 #define NISNAN(x) ((x) == (x))  // opposite of ISNAN for doubles
+// Faster than Rinternals version (which uses math library version)
+#undef ISNAN
+#define ISNAN(x) ((x) != (x))
 
 void matCopyAttr(SEXP out, SEXP x, SEXP Rdrop, int ng);
 void DFcopyAttr(SEXP out, SEXP x, int ng);
