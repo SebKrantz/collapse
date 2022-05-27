13fa27a73246fb16f2f6f403a7fbdb58d06245fa
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-05-27 03:45:14 +0200

    Further PROTECT fixes.

--- src/data.table_subset.c
+++ src/data.table_subset.c
@@ -449,8 +449,10 @@ SEXP subsetCols(SEXP x, SEXP cols, SEXP checksf) { // SEXP fretall
   if(oxl && INHERITS(x, char_datatable)) {
     setAttrib(ans, sym_datatable_locked, R_NilValue);
     // int n = asInteger(GetOption1(sym_collapse_DT_alloccol));
-    UNPROTECT(nprotect); // This needs to be here !! (asInteger and GetOption1 are allocating functions)
-    return shallow(ans, R_NilValue, ncol + 100); // n // 1024 is data.table default..
+    // UNPROTECT(nprotect); // This needs to be here !! (asInteger and GetOption1 are allocating functions)
+    SEXP res = shallow(ans, R_NilValue, ncol + 100); // n // 1024 is data.table default..
+    UNPROTECT(nprotect);
+    return res;
     // setselfref(ans); // done by shallow
   }
   UNPROTECT(nprotect);
@@ -576,8 +578,9 @@ SEXP subsetDT(SEXP x, SEXP rows, SEXP cols, SEXP checkrows) { // , SEXP fastret
     setAttrib(ans, sym_sorted, R_NilValue);
     setAttrib(ans, sym_datatable_locked, R_NilValue);
     // int n = asInteger(GetOption1(sym_collapse_DT_alloccol));
+    SEXP res = shallow(ans, R_NilValue, ncol + 100); // n // 1024 is data.table default..
     UNPROTECT(nprotect); // This needs to be here !! (asInteger and GetOption1 are allocating functions)
-    return shallow(ans, R_NilValue, ncol + 100); // n // 1024 is data.table default..
+    return res;
     // setselfref(ans); // done by shallow
   }
   UNPROTECT(nprotect);
