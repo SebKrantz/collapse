47ee623cb69b9b04d3cdc9f1dd0a0d0208f93d63
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-06-11 13:31:19 +0200

    don't need long long here.

--- src/kit_dup.c
+++ src/kit_dup.c
@@ -432,7 +432,7 @@ int dupVecSecond(int *restrict pidx, int *restrict pans_i, SEXP x, const int n, 
   case INTSXP: {
     const int *restrict px = INTEGER(x);
     for (int i = 0; i != n; ++i) {
-      id = HASH((unsigned long long)px[i] * (unsigned)pidx[i], K) + pidx[i]; // Need multiplication here instead of bitwise, see your benchmark with 100 mio. obs where second group is just sample.int(1e4, 1e8, T), there bitwise is very slow!!
+      id = HASH((unsigned)px[i] * (unsigned)pidx[i], K) + pidx[i]; // Need multiplication here instead of bitwise, see your benchmark with 100 mio. obs where second group is just sample.int(1e4, 1e8, T), there bitwise is very slow!!
       while(h[id]) {  // However multiplication causes signed integer overflow... UBSAN error.
         hid = h[id]-1;
         if(px[hid] == px[i] && pidx[hid] == pidx[i]) {
