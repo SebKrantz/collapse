c21bb9cca1b8cc4a565cff14d0f761c68f1f82d5
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-12-27 14:12:24 +0100

    fquantile also testing na.rm = FALSE option.

--- tests/testthat/test-fquantile.R
+++ tests/testthat/test-fquantile.R
@@ -7,12 +7,16 @@ for(x in mtcars) {
   for(o in list(NULL, radixorder(x))) {
     for(Qprobs in list(probs1, probs2)) {
       for(t in 5:9) {
-        expect_equal(fquantile(x, Qprobs, type = t, o = o),
-                      quantile(x, Qprobs, type = t))
+        expect_true(all_obj_equal(
+                    fquantile(x, Qprobs, type = t, o = o),
+                    fquantile(x, Qprobs, type = t, o = o, na.rm = FALSE),
+                    quantile(x, Qprobs, type = t)))
         for(j in 1:3) {
           w = rep(j + rnorm(1, sd = 0.05), 32)
-          expect_equal(fquantile(x, Qprobs, type = t),
-                       fquantile(x, Qprobs, type = t, w = w, o = o))
+          expect_true(all_obj_equal(
+                       fquantile(x, Qprobs, type = t),
+                       fquantile(x, Qprobs, type = t, w = w, o = o, na.rm = FALSE),
+                       fquantile(x, Qprobs, type = t, w = w, o = o)))
         }
       }
     }
@@ -21,16 +25,20 @@ for(x in mtcars) {
 
 expect_equal(.quantile(1:2), c(1.00, 1.25, 1.50, 1.75, 2.00))
 expect_equal(.quantile(1:3), c(1.0, 1.5, 2.0, 2.5, 3.0))
+expect_equal(.quantile(1:2, na.rm = FALSE), c(1.00, 1.25, 1.50, 1.75, 2.00))
+expect_equal(.quantile(1:3, na.rm = FALSE), c(1.0, 1.5, 2.0, 2.5, 3.0))
 
-for(t in 5:9) {
-  expect_equal(.quantile(0, type = t), c(0,0,0,0,0))
-  expect_equal(.quantile(c(0, 0), type = t), c(0,0,0,0,0))
-  expect_equal(.quantile(c(0, 0, 0), type = t), c(0,0,0,0,0))
-  expect_equal(.quantile(0L, type = t), rep.int(0L, 5))
-  expect_equal(.quantile(c(0L, 0L), type = t), rep.int(0L, 5))
-  expect_equal(.quantile(c(0L, 0L, 0L), type = t), rep.int(0L, 5))
-  expect_equal(.quantile(numeric(0), type = t), rep(NA_real_, 5))
-  expect_equal(.quantile(integer(0), type = t), rep(NA_real_, 5))
+for(na_rm in c(TRUE, FALSE)) {
+  for(t in 5:9) {
+    expect_equal(.quantile(0, type = t, na.rm = na_rm), c(0,0,0,0,0))
+    expect_equal(.quantile(c(0, 0), type = t, na.rm = na_rm), c(0,0,0,0,0))
+    expect_equal(.quantile(c(0, 0, 0), type = t, na.rm = na_rm), c(0,0,0,0,0))
+    expect_equal(.quantile(0L, type = t, na.rm = na_rm), rep.int(0L, 5))
+    expect_equal(.quantile(c(0L, 0L), type = t, na.rm = na_rm), rep.int(0L, 5))
+    expect_equal(.quantile(c(0L, 0L, 0L), type = t, na.rm = na_rm), rep.int(0L, 5))
+    expect_equal(.quantile(numeric(0), type = t, na.rm = na_rm), rep(NA_real_, 5))
+    expect_equal(.quantile(integer(0), type = t, na.rm = na_rm), rep(NA_real_, 5))
+  }
 }
 
 for(x in na_insert(airquality, 0.05)) {
