87104d29df65e2c9035ea4075eab2363a6be77be
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2023-09-08 14:28:58 +0200

    Adding "on.cols" attribute to join column.

--- R/join.R
+++ R/join.R
@@ -220,6 +220,7 @@ join <- function(x, y,
                  full = structure(vec(list(is.na(m) + 1L, alloc(3L, fnrow(res)-length(m)))), levels = c(matched, x_name, y_name), class = c("factor", "na.included")),
                  inner =, semi = structure(alloc(1L, fnrow(res)), levels = matched, class = c("factor", "na.included")),
                  anti = structure(alloc(1L, fnrow(res)), levels = x_name, class = c("factor", "na.included")))
+    attr(mc, "on.cols") <- `names<-`(list(xon, `names<-`(on, NULL)), c(x_name, y_name))
     mc_name <- if(is.character(column)) column else ".join"
     if(keep.col.order == 1L) res[[mc_name]] <- mc
     else res <- c(res[ixon], `names<-`(list(mc), mc_name), res[-ixon])
