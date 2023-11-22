320c1f513a3fe79fd9353a5fc686c719ea4c4350
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2023-11-22 10:46:53 +0000

    Adding C implementation for replace_outliers.

--- src/ExportSymbols.c
+++ src/ExportSymbols.c
@@ -129,6 +129,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"C_pivot_long", (DL_FUNC) &pivot_long, 3},
   {"C_pivot_wide", (DL_FUNC) &pivot_wide, 5},
   {"C_sort_merge_join", (DL_FUNC) &sort_merge_join, 4},
+  {"C_replace_outliers", (DL_FUNC) &replace_outliers, 5},
   // {"C_aschar", (DL_FUNC) &CasChar, 1},
   {"C_subsetDT", (DL_FUNC) &subsetDT, 4},
   {"C_subsetVector", (DL_FUNC) &subsetVector, 3},
--- src/collapse_c.h
+++ src/collapse_c.h
@@ -100,6 +100,7 @@ void writeValueByIndex(SEXP target, SEXP source, const int from, SEXP index);
 SEXP pivot_long(SEXP data, SEXP ind, SEXP idcol);
 SEXP pivot_wide(SEXP index, SEXP id, SEXP column, SEXP fill, SEXP Rnthreads);
 SEXP sort_merge_join(SEXP x, SEXP table, SEXP ot, SEXP count);
+SEXP replace_outliers(SEXP x, SEXP limits, SEXP value, SEXP single_limit, SEXP set);
 // fnobs rewritten in C:
 SEXP fnobsC(SEXP x, SEXP Rng, SEXP g);
 SEXP fnobsmC(SEXP x, SEXP Rng, SEXP g, SEXP Rdrop);
