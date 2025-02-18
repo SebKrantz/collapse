81c57d055aac7fc31c384cc3b9d30fe7f9388d41
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2025-02-19 00:37:12 +0100

    Add require argument to join().

--- R/join.R
+++ R/join.R
@@ -22,6 +22,7 @@ join <- function(x, y,
                  keep.col.order = TRUE,
                  drop.dup.cols = FALSE,
                  verbose = .op[["verbose"]],
+                 require = NULL, # E.g. require = list(x = 0.9, y = 0.8, on.fail = "error")
                  column = NULL,
                  attr = NULL, ...) { # method = c("hash", "radix") -> implicit to sort...
 
@@ -59,7 +60,7 @@ join <- function(x, y,
 
   # Matching step
   rjoin <- switch(how, right = TRUE, FALSE)
-  count <- verbose || validate != "m:m" || length(attr)
+  count <- verbose || validate != "m:m" || length(attr) || length(require)
 
   if(sort) {
     if(rjoin) {
@@ -115,43 +116,39 @@ join <- function(x, y,
     }
   }
 
-  if(verbose) {
-    Nx <- length(if(multiple) mi else m)
-    nx <- Nx - attr(m, "N.nomatch")
-    ny <- attr(m, "N.distinct")
-    Ny <- attr(m, "N.groups")
-    if(verbose == 2L) {
-      cin_x <- paste0(xon, ":", vclasses(x[ixon], FALSE))
-      cin_y <- paste0(on, ":", vclasses(y[iyon], FALSE))
-    } else {
-      cin_x <- xon
-      cin_y <- on
+  if(verbose || length(require)) {
+    Nx <- if(rjoin) attr(m, "N.groups") else length(if(multiple) mi else m)
+    Ny <- if(rjoin) length(if(multiple) mi else m) else attr(m, "N.groups")
+    nx <- if(rjoin) attr(m, "N.distinct") else Nx - attr(m, "N.nomatch")
+    ny <- if(rjoin) Ny - attr(m, "N.nomatch") else attr(m, "N.distinct")
+    if(length(require)) {
+      if(length(require$x) && require$x > nx/Nx) switch_msg(sprintf("Matched %#.1f%% of records in table %s (x), but %#.1f%% is required", nx/Nx*100, x_name, require$x*100), require$on.fail)
+      if(length(require$y) && require$y > ny/Ny) switch_msg(sprintf("Matched %#.1f%% of records in table %s (y), but %#.1f%% is required", ny/Ny*100, y_name, require$y*100), require$on.fail)
     }
-    xstat <- paste0(nx, "/", Nx, " (", signif(nx/Nx*100, 3), "%)")
-    ystat <- paste0(ny, "/", Ny, " (", signif(ny/Ny*100, 3), "%)")
-    if(rjoin) {
-      tmp <- ystat
-      ystat <- xstat
-      xstat <- tmp
+    if(verbose) {
+      cin_x <- if(verbose == 2L) paste0(xon, ":", vclasses(x[ixon], FALSE)) else xon
+      cin_y <- if(verbose == 2L) paste0(on, ":", vclasses(y[iyon], FALSE)) else on
+      xstat <- paste0(nx, "/", Nx, " (", signif(nx/Nx*100, 3), "%)")
+      ystat <- paste0(ny, "/", Ny, " (", signif(ny/Ny*100, 3), "%)")
+      if(multiple) {
+        validate <- switch(validate,
+          "1:1" = "1:1",
+          "1:m" = paste0("1:", round(ny / attr(mi, "N.distinct"), 2)),
+          "m:1" = paste0(round(nx / attr(mi, "N.distinct"), 2), ":1"),
+          "m:m" = paste(round(c(nx, ny) / attr(mi, "N.distinct"), 2), collapse = ":"))
+      } else {
+        validate <- switch(validate,
+          "1:1" = "1:1",
+          "1:m" = paste0("1:", if(rjoin) round(ny / nx, 2) else "1st"),
+          "m:1" = paste0(if(rjoin) "1st" else round(nx / ny, 2), ":1"),
+          "m:m" = if(rjoin) paste0("1st:", round(ny / nx, 2)) else paste0(round(nx / ny, 2), ":1st"))
+      }
+      cat(how, " join: ",
+          x_name, "[", paste(cin_x, collapse = ", "), "] ",
+          xstat, " <", validate , "> ",
+          y_name, "[", paste(cin_y, collapse = ", "), "] ",
+          ystat, "\n", sep = "")
     }
-    if(multiple) {
-      validate <- switch(validate,
-        "1:1" = "1:1",
-        "1:m" = paste0("1:", round((if(rjoin) nx else ny) / attr(mi, "N.distinct"), 2)),
-        "m:1" = paste0(round((if(rjoin) ny else nx) / attr(mi, "N.distinct"), 2), ":1"),
-        "m:m" = paste(round(c(nx, ny)[if(rjoin) 2:1 else 1:2] / attr(mi, "N.distinct"), 2), collapse = ":"))
-    } else {
-      validate <- switch(validate,
-        "1:1" = "1:1",
-        "1:m" = paste0("1:", if(rjoin) round(nx / ny, 2) else "1st"),
-        "m:1" = paste0(if(rjoin) "1st" else round(nx / ny, 2), ":1"),
-        "m:m" = paste(c(round(nx / ny, 2), "1st")[if(rjoin) 2:1 else 1:2], collapse = ":"))
-    }
-    cat(how, " join: ",
-        x_name, "[", paste(cin_x, collapse = ", "), "] ",
-        xstat, " <", validate , "> ",
-        y_name, "[", paste(cin_y, collapse = ", "), "] ",
-        ystat, "\n", sep = "")
   }
 
   # Check for duplicate columns and suffix as needed
